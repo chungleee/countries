a0642f81eba8149ce629749af5321a886b02b3e4
Leon <leon.karho.chung@gmail.com>
2020-09-09 15:25:32 -0400

    dark mode state + dark mode toggler func + pass dark mode state to children

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import CountryPage from "./components/CountryPage";
 import CountryList from "./components/CountryList";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    console.log(darkMode);
+  }, []);
+
   return (
     <Router>
-      <div className="bg-primaryWhite text-darkBlue">
-        <Header />
-        <main className="px-8 bg-veryLightGray">
-          <Route exact path="/" component={CountryList} />
-          <Route path="/country/:name" component={CountryPage} />
+      <div
+        className={`bg-primaryWhite ${
+          darkMode ? "text-primaryWhite" : "text-darkBlue"
+        }`}
+      >
+        <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+        <main
+          className={`px-8 bg-veryLightGray ${
+            darkMode ? "bg-darkModeDarkBlue" : null
+          }`}
+        >
+          <Route
+            exact
+            path="/"
+            render={(props) => {
+              return <CountryList {...props} darkMode={darkMode} />;
+            }}
+          />
+          <Route
+            path="/country/:name"
+            render={(props) => {
+              return <CountryPage {...props} darkMode={darkMode} />;
+            }}
+          />
         </main>
       </div>
     </Router>
