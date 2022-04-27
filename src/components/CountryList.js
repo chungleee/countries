d56ee6b4a8a8342e5c38235de269fd07081593ac
Leon <leon.karho.chung@gmail.com>
2022-04-27 15:08:16 -0400

    update api endpoints

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -7,80 +7,81 @@ import SearchBar from "./SearchBar";
 import SelectDropdown from "./SelectDropdown";
 
 const CountryList = ({ darkMode }) => {
-  const [countries, setCountries] = useState([]);
-  const [value, setValue] = useState("");
-  const [region, setRegion] = useState("");
+	const [countries, setCountries] = useState([]);
+	const [value, setValue] = useState("");
+	const [region, setRegion] = useState("");
 
-  const handleInputValue = (event) => {
-    setValue(event.target.value);
-  };
+	const handleInputValue = (event) => {
+		setValue(event.target.value);
+	};
 
-  const handleSelectByRegion = (event) => {
-    setRegion(event.target.value);
-  };
+	const handleSelectByRegion = (event) => {
+		setRegion(event.target.value);
+	};
 
-  useEffect(() => {
-    const getCountries = async () => {
-      const response = await ky
-        .get("https://restcountries.eu/rest/v2/all")
-        .json();
+	useEffect(() => {
+		const getCountries = async () => {
+			const response = await ky
+				// .get("https://restcountries.com/v3.1/all")
+				.get("https://restcountries.com/v2/all")
+				.json();
+			console.log("response: ", response);
+			setCountries(response);
+		};
 
-      setCountries(response);
-    };
+		getCountries();
+	}, []);
 
-    getCountries();
-  }, []);
-
-  return (
-    <div>
-      <div className="pt-6 md:flex md:justify-between">
-        <SearchBar darkMode={darkMode} handleInputValue={handleInputValue} />
-        <SelectDropdown
-          darkMode={darkMode}
-          handleSelectByRegion={handleSelectByRegion}
-        />
-      </div>
-      <div className="pb-4 pt-4 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-4">
-        {!countries.length ? (
-          <div>Loading...</div>
-        ) : (
-          countries
-            .filter((country) => {
-              if (value.length) {
-                return country.name.toLowerCase().includes(value.toLowerCase());
-              } else if (region.length) {
-                return country.region.includes(region);
-              } else {
-                return country;
-              }
-            })
-            .map((country) => {
-              let key = nanoid();
-              return (
-                <Link
-                  key={key}
-                  to={{
-                    pathname: `/country/${country.name}`,
-                    state: {
-                      alpha3Code: country.alpha3Code,
-                    },
-                  }}
-                >
-                  <CountryCard
-                    darkMode={darkMode}
-                    capital={country.capital}
-                    flag={country.flag}
-                    name={country.name}
-                    population={country.population}
-                    region={country.region}
-                  />
-                </Link>
-              );
-            })
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='pt-6 md:flex md:justify-between'>
+				<SearchBar darkMode={darkMode} handleInputValue={handleInputValue} />
+				<SelectDropdown
+					darkMode={darkMode}
+					handleSelectByRegion={handleSelectByRegion}
+				/>
+			</div>
+			<div className='pb-4 pt-4 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-4'>
+				{!countries.length ? (
+					<div>Loading...</div>
+				) : (
+					countries
+						.filter((country) => {
+							if (value.length) {
+								return country.name.toLowerCase().includes(value.toLowerCase());
+							} else if (region.length) {
+								return country.region.includes(region);
+							} else {
+								return country;
+							}
+						})
+						.map((country) => {
+							let key = nanoid();
+							return (
+								<Link
+									key={key}
+									to={{
+										pathname: `/country/${country.name}`,
+										state: {
+											alpha3Code: country.alpha3Code,
+										},
+									}}
+								>
+									<CountryCard
+										darkMode={darkMode}
+										capital={country.capital}
+										flag={country.flag}
+										name={country.name}
+										population={country.population}
+										region={country.region}
+									/>
+								</Link>
+							);
+						})
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default CountryList;
--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -4,158 +4,159 @@ import LeftArrowIcon from "./icons/LeftArrowIcon";
 import ky from "ky";
 
 const CountryPage = ({ location, darkMode }) => {
-  let code = location.state.alpha3Code;
-  const [country, setCountry] = useState({});
-  const [borderCountries, setBorderCountries] = useState([]);
+	let code = location.state.alpha3Code;
+	const [country, setCountry] = useState({});
+	const [borderCountries, setBorderCountries] = useState([]);
 
-  useEffect(() => {
-    const getCountryByCode = async (code) => {
-      const country = await ky
-        .get(`https://restcountries.eu/rest/v2/alpha/${code}`)
-        .json();
+	useEffect(() => {
+		const getCountryByCode = async (code) => {
+			const country = await ky
+				// .get(`https://restcountries.com/v3.1/alpha/${code}`)
+				.get(`https://restcountries.com/v2/alpha/${code}`)
+				.json();
 
-      setCountry(country);
-      console.log("country", country);
-    };
+			setCountry(country);
+			console.log("country", country);
+		};
 
-    setCountry(getCountryByCode(code));
-  }, [code]);
+		setCountry(getCountryByCode(code));
+	}, [code]);
 
-  useEffect(() => {
-    if (country?.borders) {
-      const borderCountryCodes = country.borders;
+	useEffect(() => {
+		if (country?.borders) {
+			const borderCountryCodes = country.borders;
 
-      const getBorderCountries = async (codes) => {
-        const promises = codes.map(async (code) => {
-          return await ky
-            .get(`https://restcountries.eu/rest/v2/alpha/${code}`)
-            .json();
-        });
+			const getBorderCountries = async (codes) => {
+				const promises = codes.map(async (code) => {
+					return await ky
+						.get(`https://restcountries.com/v2/alpha/${code}`)
+						.json();
+				});
 
-        const responses = await Promise.all(promises);
+				const responses = await Promise.all(promises);
 
-        setBorderCountries(responses);
-      };
+				setBorderCountries(responses);
+			};
 
-      getBorderCountries(borderCountryCodes);
-    }
-  }, [country]);
+			getBorderCountries(borderCountryCodes);
+		}
+	}, [country]);
 
-  const {
-    flag,
-    name,
-    nativeName,
-    population,
-    region,
-    subregion,
-    capital,
-    topLevelDomain,
-    currencies,
-    languages,
-  } = country;
+	const {
+		flag,
+		name,
+		nativeName,
+		population,
+		region,
+		subregion,
+		capital,
+		topLevelDomain,
+		currencies,
+		languages,
+	} = country;
 
-  return (
-    <div className="pt-8">
-      <Link
-        className={`bg-white border-solid inline-flex mb-8 px-6 py-4 shadow ${
-          darkMode ? "bg-darkBlue" : null
-        }`}
-        to="/"
-      >
-        <span className="mr-3">
-          <LeftArrowIcon />
-        </span>
-        Back
-      </Link>
+	return (
+		<div className='pt-8'>
+			<Link
+				className={`bg-white border-solid inline-flex mb-8 px-6 py-4 shadow ${
+					darkMode ? "bg-darkBlue" : null
+				}`}
+				to='/'
+			>
+				<span className='mr-3'>
+					<LeftArrowIcon />
+				</span>
+				Back
+			</Link>
 
-      {!Object.keys(country).length ? (
-        <div>...Loading...</div>
-      ) : (
-        <div className="py-8 md:flex md:items-center md:justify-between">
-          <div className="md:w-1/2">
-            <img src={flag} alt={`${name}'s flag`} />
-          </div>
-          <div className="md:w-1/2 md:pl-12">
-            <h1 className="font-bold my-6 text-xl">{name}</h1>
-            <div className="font-semibold leading-8 md:flex md:flex-col">
-              <div className="md:flex md:justify-between">
-                <div className="mb-6">
-                  <p>
-                    Native Name:{" "}
-                    <span className="font-normal">{nativeName}</span>
-                  </p>
-                  <p>
-                    Population:{" "}
-                    <span className="font-normal">
-                      {Number(population).toLocaleString()}
-                    </span>
-                  </p>
-                  <p>
-                    Region: <span className="font-normal">{region}</span>
-                  </p>
-                  <p>
-                    Sub Region: <span className="font-normal">{subregion}</span>
-                  </p>
-                  <p>
-                    Capital: <span className="font-normal">{capital}</span>
-                  </p>
-                </div>
-                <div className="mb-6">
-                  <p>
-                    Top Level Domain:{" "}
-                    <span className="font-normal">{topLevelDomain[0]}</span>
-                  </p>
-                  <p>
-                    Currencies:{" "}
-                    <span className="font-normal">{currencies[0].code}</span>
-                  </p>
-                  <p>
-                    Languages:{" "}
-                    <span className="font-normal">
-                      {languages
-                        .map((language) => {
-                          return language.name;
-                        })
-                        .join(", ")}
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <div>
-                <h1 className="mb-4">Border Countries:</h1>
-                <ul className="flex flex-wrap">
-                  {!borderCountries.length ? (
-                    <div>...Loading...</div>
-                  ) : (
-                    borderCountries.map((country) => {
-                      return (
-                        <li
-                          className={`bg-white font-normal mr-4 mb-4 px-4 py-2 shadow ${
-                            darkMode ? "bg-darkBlue" : null
-                          }`}
-                        >
-                          <Link
-                            to={{
-                              pathname: `/country/${country.name}`,
-                              state: {
-                                alpha3Code: country.alpha3Code,
-                              },
-                            }}
-                          >
-                            {country.name}
-                          </Link>
-                        </li>
-                      );
-                    })
-                  )}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+			{!Object.keys(country).length ? (
+				<div>...Loading...</div>
+			) : (
+				<div className='py-8 md:flex md:items-center md:justify-between'>
+					<div className='md:w-1/2'>
+						<img src={flag} alt={`${name}'s flag`} />
+					</div>
+					<div className='md:w-1/2 md:pl-12'>
+						<h1 className='font-bold my-6 text-xl'>{name}</h1>
+						<div className='font-semibold leading-8 md:flex md:flex-col'>
+							<div className='md:flex md:justify-between'>
+								<div className='mb-6'>
+									<p>
+										Native Name:{" "}
+										<span className='font-normal'>{nativeName}</span>
+									</p>
+									<p>
+										Population:{" "}
+										<span className='font-normal'>
+											{Number(population).toLocaleString()}
+										</span>
+									</p>
+									<p>
+										Region: <span className='font-normal'>{region}</span>
+									</p>
+									<p>
+										Sub Region: <span className='font-normal'>{subregion}</span>
+									</p>
+									<p>
+										Capital: <span className='font-normal'>{capital}</span>
+									</p>
+								</div>
+								<div className='mb-6'>
+									<p>
+										Top Level Domain:{" "}
+										<span className='font-normal'>{topLevelDomain[0]}</span>
+									</p>
+									<p>
+										Currencies:{" "}
+										<span className='font-normal'>{currencies[0].code}</span>
+									</p>
+									<p>
+										Languages:{" "}
+										<span className='font-normal'>
+											{languages
+												.map((language) => {
+													return language.name;
+												})
+												.join(", ")}
+										</span>
+									</p>
+								</div>
+							</div>
+							<div>
+								<h1 className='mb-4'>Border Countries:</h1>
+								<ul className='flex flex-wrap'>
+									{!borderCountries.length ? (
+										<div>...Loading...</div>
+									) : (
+										borderCountries.map((country) => {
+											return (
+												<li
+													className={`bg-white font-normal mr-4 mb-4 px-4 py-2 shadow ${
+														darkMode ? "bg-darkBlue" : null
+													}`}
+												>
+													<Link
+														to={{
+															pathname: `/country/${country.name}`,
+															state: {
+																alpha3Code: country.alpha3Code,
+															},
+														}}
+													>
+														{country.name}
+													</Link>
+												</li>
+											);
+										})
+									)}
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default CountryPage;
